8fb9d196fee39ee3b1a86bc0afe3548141a8e587
Lucas Paes <50775822+LucasTMP@users.noreply.github.com>
2022-11-13 22:31:56 -0300

    ➕: Adicionando pacotes e dependencias

---
+++ src/Template.Validator.Api/Controllers/Bases/DefaultResponse.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace Template.Validator.Api.Controllers.Bases
+{
+    public class DefaultResponse
+    {
+        public HttpStatusCode StatusCode { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Environments { get; private set; }
+        public bool Success { get; private set; }
+        public object Data { get; private set; }
+        public IEnumerable<string> Errors { get; private set; }
+
+        public DefaultResponse(HttpStatusCode statusCode, bool success)
+        {
+            StatusCode = statusCode;
+            Success = success;
+            Date = DateTime.Now;
+            Environments = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        }
+
+        public DefaultResponse(HttpStatusCode statusCode, bool success, object data) : this(statusCode, success) =>
+            Data = data;
+
+        public DefaultResponse(HttpStatusCode statusCode, bool success, IEnumerable<string> errors) : this(statusCode, success) =>
+            Errors = errors;
+
+        public DefaultResponse(HttpStatusCode statusCode, bool success, object data, IEnumerable<string> errors) : this(statusCode, success, data) =>
+            Errors = errors;
+    }
+}
---
+++ src/Template.Validator.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Template.Validator.Core
+{
+    public class Class1
+    {
+
+    }
+}
