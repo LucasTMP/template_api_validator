83e86f617ebdd27ece55c689108387e76c1129ce
Thone <46010284+thonecardoso@users.noreply.github.com>
2022-11-15 16:28:40 -0300

    feat: Adicionado interface de serviço do client

---
+++ src/Template.Validator.Api/Contracts/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using Template.Validator.Domain.Models;
+
+namespace Template.Validator.Api.Contracts;
+
+public interface IClientService
+{
+    Task<Client> Add(Client client);
+    Task<Client> Update(Client client);
+    Task Delete(string id);
+    Task<List<Client>> GetAll();
+    Task<Client> Get(string id);
+
+}
